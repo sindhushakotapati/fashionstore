b08e499ca5876064a10ef9edcdf9c50cac3ac6fc
Sindhusha Kotapati <sindhusha.kotapati@gmail.com>
2019-03-17 19:46:08 -0400

    Add files via upload

---
+++ precache-manifest.fbb7230c56c3437aeb4b385e4f8a2a52.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9f7e506d7455da27ce24e474c84a8e0a",
+    "url": "/fashionstore/static/media/logo.9f7e506d.svg"
+  },
+  {
+    "revision": "8509be8b5eb6600084b6",
+    "url": "/fashionstore/static/js/runtime~main.a649724b.js"
+  },
+  {
+    "revision": "073c343c0e5713a82e45",
+    "url": "/fashionstore/static/js/main.63bcd582.chunk.js"
+  },
+  {
+    "revision": "d409d280bc47b78436ec",
+    "url": "/fashionstore/static/js/2.04b9266e.chunk.js"
+  },
+  {
+    "revision": "073c343c0e5713a82e45",
+    "url": "/fashionstore/static/css/main.010e5ba0.chunk.css"
+  },
+  {
+    "revision": "d409d280bc47b78436ec",
+    "url": "/fashionstore/static/css/2.30c96837.chunk.css"
+  },
+  {
+    "revision": "8f76292bab47e580c64167107050d856",
+    "url": "/fashionstore/index.html"
+  }
+];
